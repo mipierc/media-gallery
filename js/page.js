b12a8920eaefb4536f25f5ea15f767af06a2c1e6
m.pierce <mpierce2602@users.noreply.github.com>
2021-10-27 14:37:15 -0400

    implements core media gallery
    missing final styling and some image descriptions

--- js/page.js
+++ js/page.js
@@ -1,4 +1,47 @@
-function test() {
-  let el = document.getElementById('test');
-  el.classList.add("test");
+function navToggle() {
+  var x = document.getElementById("main-nav");
+    if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
 }
+var slideIndex = 1;
+
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  var indicator = document.getElementsByClassName("indicator");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < indicator.length; i++) {
+      indicator[i].className = indicator[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  indicator[slideIndex-1].className += " active";
+}
+
+
+$(".button").on("click", function() {
+  var modal = $(this).data("modal");
+  $(modal).show();
+});
+$(".modal").on("click", function(e) {
+  var className = e.target.className;
+  if(className === "modal" || className === "close"){
+    $(this).closest(".modal").hide();
+  }
+});
